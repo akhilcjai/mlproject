067c007cb5afd8447f22b7cf0f4e4cd3890e3f9a
akhilcj90 <akhilcj@ymail.com>
2023-04-30 18:23:27 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_details:sys):
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -15,5 +16,4 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-    
     
